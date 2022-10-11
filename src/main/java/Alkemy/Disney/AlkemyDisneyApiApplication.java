24d4f6343f65f17b79644c7c8f09da735fdaaa9c
Francisco Mora <fmorajara@gmail.com>
2022-10-11 19:12:18 -0300

    Inicio desafío Alkemy Back End

---
+++ src/main/java/Alkemy/Disney/AlkemyDisneyApiApplication.java
@@ -0,0 +1,13 @@
+package Alkemy.Disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyDisneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyDisneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Alkemy/Disney/AlkemyDisneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Alkemy.Disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyDisneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
